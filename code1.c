d360f38020c55fe4ebf33a1cd3f191419d1fa82c
c <khilji@kth.se>
2021-09-19 19:57:20 +0500

    first commit

---
+++ code1.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main()
+
+{
+	printf("for more info pull changes from github\n");
+	int * y;
+	y = new int; 
+	//printf("garbage value of pointer %d\n",*y);
+	printf("address of pointer %d\n",&y);
+
+	return 0;
+	
+}
